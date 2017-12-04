61af979a84b4a8e0da96357d9aba2fdb81af56c3
chenlingjie <chenlingjie@cd121.com>
2017-12-04 18:00:19 +0800

    xg

--- src/routes/Home/announcement.js
+++ src/routes/Home/announcement.js
@@ -2,19 +2,21 @@
  * Created by xiaobxia on 2017/12/4.
  */
 import React, {PureComponent} from 'react'
-import {Carousel} from 'antd';
-import bgPlant from '../../assets/bg_plant.svg'
+import {Carousel, Alert} from 'antd';
 
 class AppAnnouncement extends PureComponent {
   render() {
     return (
       <div className="announcement-wrap">
-        <Carousel effect="fade">
-          <div><img src={bgPlant} alt=""/></div>
-          <div><img src={bgPlant} alt=""/></div>
-          <div><img src={bgPlant} alt=""/></div>
-          <div><img src={bgPlant} alt=""/></div>
-        </Carousel>
+        <Alert
+          message={
+            <Carousel dots={false} vertical>
+              <div>公告1</div>
+              <div>公告2</div>
+              <div>公告3</div>
+            </Carousel>
+          }
+          type="success"/>
       </div>
     );
   }
