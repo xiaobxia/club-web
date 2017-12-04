d2898b61dcf15dc58360aae93aefb9c05a3e38a4
chenlingjie <chenlingjie@cd121.com>
2017-12-04 17:50:26 +0800

    xg

--- src/routes/Home/announcement.js
+++ src/routes/Home/announcement.js
@@ -3,16 +3,17 @@
  */
 import React, {PureComponent} from 'react'
 import {Carousel} from 'antd';
+import bgPlant from '../../assets/bg_plant.svg'
 
 class AppAnnouncement extends PureComponent {
   render() {
     return (
       <div className="announcement-wrap">
         <Carousel effect="fade">
-          <div><h3>1</h3></div>
-          <div><h3>2</h3></div>
-          <div><h3>3</h3></div>
-          <div><h3>4</h3></div>
+          <div><img src={bgPlant} alt=""/></div>
+          <div><img src={bgPlant} alt=""/></div>
+          <div><img src={bgPlant} alt=""/></div>
+          <div><img src={bgPlant} alt=""/></div>
         </Carousel>
       </div>
     );
