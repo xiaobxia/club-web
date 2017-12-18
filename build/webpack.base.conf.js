37c9983ad0579034eeedf40a0ef80dcbfa63fb34
chenlingjie <chenlingjie@cd121.com>
2017-12-18 09:29:29 +0800

    xg

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -19,7 +19,7 @@ module.exports = {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.idCdn ? config.build.cdnPublicPath : config.build.assetsPublicPath
+      ? config.build.ifCdn ? config.build.cdnPublicPath : config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
   resolve: {
